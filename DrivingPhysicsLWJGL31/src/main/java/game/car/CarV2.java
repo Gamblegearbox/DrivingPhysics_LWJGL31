7073b98416ed3e4bc18d4b74086695e289ebf371
PMichaelsen <gamblegearbox@googlemail.com>
2017-02-22 09:18:27 +0100

    converted forces back to vectors and added map for lateral tire forces

--- DrivingPhysicsLWJGL31/src/main/java/game/car/CarV2.java
+++ DrivingPhysicsLWJGL31/src/main/java/game/car/CarV2.java
@@ -4,6 +4,9 @@ import org.joml.Math;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarV2 {
 
     private final Engine engine;
@@ -12,7 +15,7 @@ public class CarV2 {
     public Vector3f position = new Vector3f(0,0,0);
     public Vector3f rotation = new Vector3f(0,0,0);
 
-    private Vector2f direction = new Vector2f(0,0);
+    private Vector2f carHeading = new Vector2f(1,0);
     private Vector2f velocity = new Vector2f(0,0);
 
     public float speed = 0;
@@ -44,29 +47,31 @@ public class CarV2 {
         steeringAngle = steeringInput * max_steeringAngle;
         speed = velocity.length();
         float maxTorque = getDrivingForce(2500, driveTrain.getGear(speed));
-        float fTraction;
+        Vector2f fTraction;
         if(brakeInput > 0)
         {
-            fTraction = -brakeTorque * brakeInput;
+            fTraction = new Vector2f(carHeading).mul(-brakeTorque * brakeInput);
         }
         else
         {
-            fTraction = maxTorque * throttleInput;
+            fTraction = new Vector2f(carHeading).mul(maxTorque * throttleInput);
         }
 
-        float fDrag = -cDrag * speed * speed;
-        float fRoll = -cRoll * speed;
-        float fLong = fTraction + fDrag + fRoll;
-        float accell = fLong / mass;
-        speed = speed + accell *interval;
+        Vector2f fDrag = new Vector2f(velocity).mul(-cDrag * speed);
+        Vector2f fRoll = new Vector2f(velocity).mul(-cRoll * speed);
+        Vector2f fLong = new Vector2f(fTraction).add(fDrag).add(fRoll);
+        Vector2f accell = new Vector2f();
+        accell.x = fLong.x / mass;
+        accell.y = fLong.y / mass;
+
+        velocity.x = velocity.x + accell.x * interval;
+        velocity.y = velocity.y + accell.y * interval;
 
         float turnRadius = wheelBase / (float)Math.sin(Math.toRadians(steeringAngle));
         float angularVelo = speed / turnRadius;
         rotation.y += angularVelo;
-        direction.x = (float) Math.cos(Math.toRadians(rotation.y));
-        direction.y = (float) -Math.sin(Math.toRadians(rotation.y));
-
-        velocity = new Vector2f(direction).mul(speed);
+        carHeading.x = (float) Math.cos(Math.toRadians(rotation.y));
+        carHeading.y = (float) -Math.sin(Math.toRadians(rotation.y));
 
         updatePosition(interval);
     }
@@ -75,6 +80,42 @@ public class CarV2 {
     {
         position.x = position.x + velocity.x * interval;
         position.z = position.z + velocity.y * interval;
+    }
+
+    private float getLateralTireForce(float slipAngle)
+    {
+        if(slipAngle > 21)
+        {
+            slipAngle = 21;
+        }
+        if(slipAngle < -21)
+        {
+            slipAngle = -21;
+        }
+        int mod = (int)(slipAngle % 3);
+        int index = (int)(slipAngle) - mod;
+        if(index < 0)
+        {
+            index = index * -1;
+        }
+
+        Map lateralForces = new HashMap<Float, Float>();// at 5KN load (500kg which is true in our case, since the car does not weight shift)
+        lateralForces.put(0,0);
+        lateralForces.put(3,6000);
+        lateralForces.put(6,5800);
+        lateralForces.put(9,5600);
+        lateralForces.put(12,5400);
+        lateralForces.put(15,5200);
+        lateralForces.put(18,5000);
+        lateralForces.put(21,4800);
+
+        // interpolate angle
+        float angle_1 = engine.torqueChart[index];
+        float angle_2 = engine.torqueChart[index + 1];
+        float force_1 = (index) * 1000;
+        float force_2 = (index + 1) * 1000;
+
+        return 0;
     }
 
     private float getDrivingForce(float rpm, int gear)
